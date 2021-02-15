fb065b1a2f7a56adccdf8ef405309e7dae9bc072
Know-Thyself <67852769+Know-Thyself@users.noreply.github.com>
2021-02-15 19:53:58 +0000

    Level 100 is completed.
    Data fetched, extracted and styled.

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 //You can edit ALL of the code here
-function setup() {
+
+let promise = fetch("https://api.tvmaze.com/shows/82/episodes")
+.then(response => response.json())
+
+promise.then(extract);
+
+function extract(obj) {
+ obj.forEach(element => {
+
+   const rootElem = document.getElementById("root");
+   
+    let expandingList = document.createElement('div', { is : 'expanding-list' })
+    rootElem.appendChild(expandingList);
+    header = document.createElement('h3');
+    expandingList.appendChild(header);
+    header.innerHTML = `${element.name} S0${element.season}E0${element.number}`;
+    img = document.createElement('img');
+    img.src = element.image.medium; 
+    expandingList.appendChild(img);
+    paragraph = document.createElement('p');
+    paragraph.innerHTML = `${element.summary}`;
+    expandingList.appendChild(paragraph);
+
+ });
+}
+
+
+/* function setup() {
+  
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+ 
 }
 
 function makePageForEpisodes(episodeList) {
@@ -10,3 +39,4 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+ */
