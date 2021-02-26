35ebe47298004cc2d760fcc3526325f7e4eada00
Biruk Kebede <biruk.key@gmail.com>
2021-02-26 05:13:36 +0000

    Shows are loaded
    I have taken a different approach and structure from what I did before and I had to comment out almost all my previous codes until I link them to the whole structure.

--- script.js
+++ script.js
@@ -2,28 +2,177 @@
 // Getting elements from the DOM 
 const rootElem = document.getElementById("root");
 const searchBar = document.querySelector(".search-bar");
+let selectShows = document.getElementById('shows');
 const parentDiv = document.getElementsByTagName('div');
-let searchResult;
+let data = [];
 
-// Getting data from API and parsing it into a json file
-let request = fetch("https://api.tvmaze.com/shows/82/episodes")
-const promisedEpisodes = request.then(response => response.json())
-console.log(promisedEpisodes)
-// And then passing the data into functions to generate content to the webpage
-const extractedEpisodes = promisedEpisodes.then(extracted)
-promisedEpisodes.then(searchedObj)
+//Getting data from API and passing it as a parameter
 
-// A function to extract and transform the data into a webpage. 
+const loadShows = async () => {
+  try {
+    const response = await fetch('https://api.tvmaze.com/shows');
+    data = await response.json();
+    console.log(data);
+    allShows(data);
+    searchEvent(data)
+  } catch (err){
+      console.error(err);
+    }
+   
+}
+
+// A function to extract data and populate the webpage. 
+function allShows(obj) {
+  obj.forEach(element => {
+   
+    let expandingList = document.createElement('div', { is : 'expanding-list' });
+    expandingList.setAttribute('class', 'expanding-div');
+    rootElem.appendChild(expandingList);
+    header = document.createElement('h2');
+    expandingList.appendChild(header);
+    header.innerHTML = `${element.name}` 
+    //S0${element.season}E0${element.number}`;
+    img = document.createElement('img');
+    img.setAttribute('class', 'episodeImage');
+    //let usableImg = Object.values(`${element.image}`);
+    img.src = element.image.medium; 
+    expandingList.appendChild(img);
+    paragraph = document.createElement('p');
+    paragraph.innerHTML = `${element.summary}`;
+    expandingList.appendChild(paragraph);
+
+    //Genres, Status, Rating and Runtime
+    let genres = document.createElement('h4');
+    //let status = document.createElement('h4');
+    let rating = document.createElement('h4');
+    //let runtime = document.createElement('h4');
+    expandingList.appendChild(genres);
+    expandingList.appendChild(rating);
+    // Giving a bit of space between words and after commas
+    let corrected = element.genres.toString().split(',').join(', ');
+  
+     
+    genres.innerHTML = `Genres: ${corrected}`
+    rating.innerHTML = `Status: ${element.status}   Rating: ${element.rating.average} Runtime: ${element.runtime}`; 
+
+    genres.style.wordSpacing = "5px";
+    rating.style.wordSpacing = "5px";
+
+    let select = document.querySelector('#shows');
+    let options = document.createElement('option');
+    options.innerHTML = `${element.name}`;
+    select.appendChild(options);
+
+    // An event listener for select option box
+    select.addEventListener('change', function(){
+      console.log(this.value);
+      let checker = document.createElement('h2');
+      checker.innerHTML = `${this.value}`;
+      // .split(' ').slice(2).join(' ');
+      
+      for (let i = 3; i < parentDiv.length; i++) {
+        if (!parentDiv[i].firstChild.innerHTML.includes(checker.innerHTML)) {
+          parentDiv[i].style.display = "none";
+
+        } 
+          else if(parentDiv[i].firstChild.innerHTML.includes(checker.innerHTML)) {
+            parentDiv[i].style.display = "flex";
+            parentDiv[i].style.width = "100%";
+            document.querySelector('.hidden-button-container').style.display = "block";
+            // once all the functionalities are done, it needs a bit of css styling for small screen sizes
+          } 
+
+      }
+
+    })
+
+  });
+ 
+}
+
+
+// An event listener to dynamically update the web page while a user is typing in the search bar.
+function searchEvent () { 
+
+  searchBar.addEventListener('keyup', (e) => {
+
+    const searchInfo = document.querySelector('.search-info');
+    const searchInfo2 = document.querySelector('.search-info2');
+    // Revealing the hidden information lines
+    searchInfo.style.display = "block";
+    searchInfo2.style.display = "block";
+
+    let searchValue = e.target.value.toLowerCase();
+   
+    let searchResult = data.filter((element) => {
+      return (element.name.toLowerCase().includes(searchValue) || element.summary.toLowerCase().includes(searchValue) || element.genres.toString().toLowerCase().includes(searchValue));
+    
+    })  
+    
+    for (let i = 3; i < parentDiv.length; i++) {
+      if (!parentDiv[i].innerHTML.toLowerCase().includes(searchValue)) {
+          parentDiv[i].style.display = "none";
+
+      } else if(parentDiv[i].innerHTML.toLowerCase().includes(searchValue)) {
+          parentDiv[i].style.display = "flexbox";
+         
+        } 
+
+    }
+  
+    if (searchValue === "") {
+      searchInfo.style.display = "none";
+      searchInfo2.style.display = "none";
+      window.location.reload();
+    }
+
+    searchInfo.innerHTML = `Displaying ${searchResult.length}/${240} Shows`;
+ 
+  });
+
+}
+
+// An event listener to go back to all episodes
+hiddenButton = document.querySelector('.hidden-button');
+
+hiddenButton.addEventListener('click', function() {
+  window.location.reload();
+
+})
+
+
+
+//window.onload = getAllShows;
+/* function setup() {
+  
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+ 
+}
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+window.onload = setup; */
+
+
+/* 
+
 function extracted(obj) {
   obj.forEach(element => {
    
     let expandingList = document.createElement('div', { is : 'expanding-list' });
+    expandingList.setAttribute('id', 'expandingDiv');
     rootElem.appendChild(expandingList);
     header = document.createElement('h3');
     expandingList.appendChild(header);
     header.innerHTML = `${element.name} S0${element.season}E0${element.number}`;
-    header.setAttribute('id', `${element.id}`)
     img = document.createElement('img');
+    img.setAttribute('class', 'episodeImage');
+    // let usableImg = Object.values(element.image);
+    // console.log(usableImg);
     img.src = element.image.medium; 
     expandingList.appendChild(img);
     paragraph = document.createElement('p');
@@ -31,10 +180,9 @@ function extracted(obj) {
     expandingList.appendChild(paragraph);
 
 
-    let select = document.querySelector('.select');
+    let select = document.querySelector('#episodes');
     let options = document.createElement('option');
     options.innerHTML = `S0${element.season}E0${element.number} - ${element.name}`;
-    options.setAttribute('id', `${element.id}`)
     select.appendChild(options);
 
     const activeOption = document.querySelector('.dropdown-menu');
@@ -42,9 +190,10 @@ function extracted(obj) {
     select.addEventListener('change', function(){
       console.log(this.value);
       let checker = document.createElement('option');
-      checker.innerHTML = (`${this.value}`).split(' ').slice(2).join(' ');;
+      checker.innerHTML = (this.value).split(' ').slice(2).join(' ');
       
       for (let i = 3; i < parentDiv.length; i++) {
+        
         if (!parentDiv[i].firstChild.innerHTML.includes(checker.innerHTML)) {
           parentDiv[i].style.display = "none";
         } 
@@ -63,18 +212,20 @@ function extracted(obj) {
 
 }
 
+
+
 // An event listener to dynamically update the web page while a user is typing in the search bar.
 function searchedObj(obj) {
   
   const searchEvent = searchBar.addEventListener('keyup', (e) => {
-  
+  // const expandingList = document.getElementById('expandingDiv');
   const searchInfo = document.querySelector('.search-info');
   const searchInfo2 = document.querySelector('.search-info2');
   searchInfo.style.display = "block";
   searchInfo2.style.display = "block";
   
     const searchValue = e.target.value.toLowerCase();
-    searchResult = obj.filter((episode) => {
+  searchResult = obj.filter((episode) => {
     return (episode.name.toLowerCase().includes(searchValue) || episode.summary.toLowerCase().includes(searchValue));
 
     })
@@ -99,24 +250,44 @@ function searchedObj(obj) {
   
 } 
 
-// An event listener to go back to all episodes
-hiddenBtnDiv = document.querySelector('.hidden-button-container');
-hiddenButton = document.querySelector('.hidden-button');
-hiddenButton.addEventListener('click', function() {
-  window.location.reload();
-})
-
-/* function setup() {
-  
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
- 
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
-
-window.onload = setup;
  */
+
+/* function selectAShow(arr) {
+  
+  arr.forEach(element => {
+    let shows = document.createElement('option'); 
+    shows.setAttribute("id", "`${element.id}`")
+    shows.innerHTML = `${element.name}`;
+    shows.setAttribute("id", `${element.id}`)
+    selectShows.append(shows);
+
+        selectShows.addEventListener('change', function() {
+        console.log(this.value);
+      
+          if (`${element.name}` === this.value) { 
+              `${element.id}`;
+              showID =`${element.id}`;
+              console.log(showID);
+              console.log(`${element.id}`);
+              let selected = document.getElementById(`${element.id}`)
+              console.log(selected);
+              console.log(selected.length);
+
+
+              fetch("https://api.tvmaze.com/shows/"+showID+"/episodes")
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+                console.log(data.length);
+                return data;
+              })
+              .then(extracted)
+              .then(searchedObj)
+                
+          }
+          
+        })
+  })
+} */
+
+window.onload = loadShows;
